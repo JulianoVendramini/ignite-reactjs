59895c4ea80001e5917abe1506b354c19750dcba
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2021-09-30 07:37:11 -0300

    importing modal do global app

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -1,10 +1,34 @@
 import { Dashboard } from "./components/Dashboard";
+import Modal from 'react-modal';
 import { Header } from "./components/Header";
 
+import { useState } from "react";
+
+Modal.setAppElement('#root');
+
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModal] = useState(false);
+    
+    function handleOpenNewTransactionModal(){
+        setIsNewTransactionModal(true);
+    }
+
+    function handleCloseNewTransactionModal(){
+        setIsNewTransactionModal(false);
+    }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>   
+      </Modal>
+
       <Dashboard />
     </>
   );
