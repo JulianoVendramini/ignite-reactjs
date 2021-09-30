978bd9ef02bbc3f6f4e0c7af44958417d72abb03
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2021-09-29 22:02:43 -0300

    importing server and creating the fake api

--- 02-dtmoney/src/index.tsx
+++ 02-dtmoney/src/index.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+
+import { createServer } from 'miragejs'
+
 import { GlobalStyle } from './styles/global';
+
+createServer({
+  routes(){
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return[
+        {
+          id: 1,
+          title: 'transaction 1',
+          amount: 400,
+          type: 'deposti',
+          category: 'food',
+          createAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
