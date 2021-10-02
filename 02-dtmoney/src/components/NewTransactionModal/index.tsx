047ec592ebea25e69f88eb3368423166595e2848
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2021-10-02 10:14:23 -0300

    getting form values

--- 02-dtmoney/src/components/NewTransactionModal/index.tsx
+++ 02-dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useState, FormEvent } from 'react';
 import Modal from 'react-modal';
+
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
@@ -13,7 +14,16 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal({ isOpen, onRequestClose} : NewTransactionModalProps){
 
+const [title, setTitle] = useState('');
+const [value, setValue] = useState(0)
+const [category, setCategory] = useState('');
+
 const [type, setType] = useState('deposit')
+
+function hadleCreateNewTransaction(event : FormEvent){
+    event.preventDefault();
+    console.log(title, value, category);
+}
     
     return(
         <Modal
@@ -31,16 +41,20 @@ const [type, setType] = useState('deposit')
             <img src={closeImg} alt="closeIcon"/>
         </button>
 
-        <Container>
+        <Container onSubmit={hadleCreateNewTransaction}>
             <h2>Cadastrar transação</h2>
 
             <input 
                 placeholder="Título"
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
             />
 
             <input 
                 type="number"
                 placeholder="Valor"
+                value={value}
+                onChange={(event) => setValue(Number(event.target.value))}
             />
 
             <TransactionTypeContainer>
@@ -67,6 +81,8 @@ const [type, setType] = useState('deposit')
 
             <input 
                 placeholder="Categoria"
+                value={category}
+                onChange={(event) => setCategory(event.target.value)}
             />
 
             <button type="submit">
