ae80bd46d60fa0dacb62ac0831265275021e3a86
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2021-10-04 07:34:53 -0300

    importing TransactionsProvider

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -4,7 +4,7 @@ import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
-import { TransactionsContext } from "./TransactionsContext";
+import { TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement('#root');
 
@@ -21,7 +21,7 @@ export function App() {
     }
 
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
 
       <Dashboard />
@@ -30,6 +30,6 @@ export function App() {
         isOpen={isNewTransactionModalOpen} 
         onRequestClose={handleCloseNewTransactionModal}
         />
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }
