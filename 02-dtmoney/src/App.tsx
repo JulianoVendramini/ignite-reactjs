0af9d26472cbb9e3f5f19b9c6768313f3a44796f
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2021-10-04 07:11:36 -0300

    importing TransactionsContext

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -1,9 +1,10 @@
-import { Dashboard } from "./components/Dashboard";
-  import Modal from 'react-modal';
-import { Header } from "./components/Header";
-
 import { useState } from "react";
+import Modal from 'react-modal';
+
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsContext } from "./TransactionsContext";
 
 Modal.setAppElement('#root');
 
@@ -20,7 +21,7 @@ export function App() {
     }
 
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
 
       <Dashboard />
@@ -29,6 +30,6 @@ export function App() {
         isOpen={isNewTransactionModalOpen} 
         onRequestClose={handleCloseNewTransactionModal}
         />
-    </>
+    </TransactionsContext.Provider>
   );
 }
