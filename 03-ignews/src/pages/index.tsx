eddea9e7bb1a8f8737bee565de961d462daf5aa7
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2021-10-25 22:59:44 -0300

    created home page

--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
@@ -6,7 +7,19 @@ export default function Home() {
     <Head>
       <title>Home ig.news</title>
     </Head>
-    <h1>Hello world</h1>
+    
+    <main className={styles.contentContainer}>
+      <section  className={styles.hero}>
+        <span>👏 Hey, welcome</span>
+        <h1>News about the <span> world.</span></h1>
+        <p>
+          Get acess to all the publications <br />
+          <span>for $9.90 monthly</span>
+        </p>
+      </section>
+
+      <img src="/images/avatar.svg" alt="Girl Coding"/>
+    </main>
     </>
   )
 }
