5ad1cc4e6754a3c08293b54b927bc169e5bf137b
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2021-11-08 08:09:32 -0300

    signIn and signOut functions and getting data from next auth

--- 03-ignews/src/components/SignInButton/index.tsx
+++ 03-ignews/src/components/SignInButton/index.tsx
@@ -1,3 +1,5 @@
+import { signIn, signOut ,useSession } from 'next-auth/client'
+
 import styles from './styles.module.scss'
 
 import { FaGithub } from 'react-icons/fa'
@@ -5,21 +7,26 @@ import { FiX } from 'react-icons/fi'
 
 export function SignInButton() {
 
-  const isUserLoggedIn = true;
+  const [session] = useSession()
 
-  return isUserLoggedIn ? (
+  console.log(session);
+  
+
+  return session ? (
     <button
     type="button"
     className={styles.signInButton}
+    onClick={() => signOut()}
   >
       <FaGithub color="#04d361"/>
-      Juliano Vendramini
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon}/>
   </button>
   ) : (
     <button
     type="button"
     className={styles.signInButton}
+    onClick={() => signIn('github')}
   >
       <FaGithub color="#eba417"/>
       Sign in with Github
